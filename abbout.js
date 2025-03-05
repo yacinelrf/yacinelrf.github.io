877913f235aafbb02d334da6491278eba7b641bb
Yacine LARFI <yacinelarfi3@gmail.com>
2025-03-05 14:30:12 +0100

    added

--- abbout.js
+++ abbout.js
@@ -1,9 +1,19 @@
-let title= document.getElementById("h1");
-let text=document.getElementById("p");
-window.addEventListener("load", function() {
+let title = document.getElementById("h1");
+let text = document.getElementById("p");
+
+function adjustPosition() {
+  if (window.matchMedia("(max-width: 768px)").matches) {
     title.style.left = "35%";
-    title.style.transition="2s";
-    text.style.left="5%";
-    text.style.transition="3s"; 
-  });
-  
+    text.style.left = "5%";
+  } else {
+    title.style.left = "50vw";
+    text.style.left = "50vw";
+  }
+
+  title.style.transition = "left 2s";
+  text.style.transition = "left 3s";
+}
+
+// Exécuter au chargement et au redimensionnement
+window.addEventListener("load", adjustPosition);
+window.addEventListener("resize", adjustPosition);
