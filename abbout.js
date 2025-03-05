2f4793224ab547920c949a44f43122cf2d84ea5e
Yacine LARFI <yacinelarfi3@gmail.com>
2025-03-05 02:26:51 +0100

    added

--- abbout.js
+++ abbout.js
@@ -1,9 +1,9 @@
 let title= document.getElementById("h1");
 let text=document.getElementById("p");
 window.addEventListener("load", function() {
-    title.style.left = "100%";
+    title.style.left = "35%";
     title.style.transition="2s";
-    text.style.left="100%";
+    text.style.left="5%";
     text.style.transition="3s"; 
   });
   
